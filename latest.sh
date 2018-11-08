ac7e6b63c49942d4fda1a8566b7edd2acbb39c0d
Jérôme Warnier <jwarnier@beeznest.net>
2018-11-08 16:27:21 +0100

    Check for availability of packages for Stretch
    Obviously, Jessie is no longer the base Debian system behind image ruby:2.4-stretch-slim, so check the right packages availability.

--- latest.sh
+++ latest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-curl -s http://sensu.global.ssl.fastly.net/apt/pool/jessie/main/s/sensu/ | grep - | cut -d'>' -f2 | cut -d'_' -f2 | sort | tail -n1
+curl -s http://sensu.global.ssl.fastly.net/apt/pool/stretch/main/s/sensu/ | grep - | cut -d'>' -f2 | cut -d'_' -f2 | sort | tail -n1
